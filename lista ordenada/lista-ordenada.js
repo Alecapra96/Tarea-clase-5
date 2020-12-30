31311d9c0cbea897f3bc752a367add9f99d70cd6
alejandro <alejandrocapraargentina@gmail.com>
2020-12-30 13:19:34 -0300

    rehize lo que se borro

--- lista ordenada/lista-ordenada.js
+++ lista ordenada/lista-ordenada.js
@@ -5,6 +5,9 @@
 // 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+let $botonCalcular = document.getElementById("botonCalcular");
+$botonCalcular.onclick = function ClickenCalcular(){
+
 const lista = document.querySelectorAll('li');
 const array =[];
 for(let i=0; i<lista.length; i++){
@@ -17,32 +20,68 @@ function promedio (array){
     for(let i=0; i<array.length;i++){
         sum +=array[i];
     }
-    let promediocalculado = sum / array.length;
-    return promediocalculado;
+    let promedioCalculado = sum / array.length;
+    return promedioCalculado;
 }
-let promediocalculado = promedio(array);
-console.log(promediocalculado);
+let promedioCalculado = promedio(array);
+
 
 function numeroMasPequeño(array){
-    let iviejo= array[0];
+    let iViejo= array[0];
     for(let i=0; i<array.length;i++){ 
-        if (array[i] < iviejo){
-            iviejo= array[i];     
+        if (array[i] < iViejo){
+            iViejo= array[i];     
         }
     }
-return iviejo
+    return iViejo
 }
-let numeromaschico = numeroMasPequeño(array);
-console.log(numeromaschico);
+let numeroMasChico = numeroMasPequeño(array);
+
 
 function numeroMasGrande(array){
-    let iviejo= array[0];
+    let iViejo= array[0];
     for(let i=0; i<array.length;i++){ 
-        if (array[i] > iviejo){
-            iviejo= array[i];     
+        if (array[i] > iViejo){
+            iViejo= array[i];     
         }
     }
-return iviejo
+return iViejo
 }
-let numeromasgrande = numeroMasGrande(array);
-console.log(numeromasgrande);
+let numeroMasGrande1 = numeroMasGrande(array);
+
+
+function numeroMasRepite(array){
+    let frecuente;
+    let max=0;
+     for(let i=0; i<array.length;i++){ 
+         let vecesRepetidos = 0;
+         for(let j=0; j<array.length;j++){    
+             if(array[j] === array[i]){    
+                 vecesRepetidos++;
+             }
+             if (vecesRepetidos>max){
+                 frecuente = array[j];
+                 max=vecesRepetidos;     
+             }
+         }       
+     }
+     return frecuente;
+}
+let numeroQueMasSeRepite = numeroMasRepite(array); 
+
+let resultado0 = document.querySelector('#resultado0');
+resultado0.textContent = "El promedio es "+promedioCalculado ;
+document.querySelector("#resultado0").style.visibility = 'visible';
+
+let resultado1 = document.querySelector('#resultado1');
+resultado1.textContent = "El numero mas pequeño es el "+numeroMasChico ;
+document.querySelector("#resultado1").style.visibility = 'visible';
+
+let resultado2 = document.querySelector('#resultado2');
+resultado2.textContent = "El numero mas grande es el "+numeroMasGrande1 ;
+document.querySelector("#resultado2").style.visibility = 'visible';
+
+let resultado3 = document.querySelector('#resultado3');
+resultado3.textContent = "El numero que mas se repite es el "+numeroQueMasSeRepite ;
+document.querySelector("#resultado3").style.visibility = 'visible';
+}
--- login bar/bienvenida.js
+++ login bar/bienvenida.js
@@ -1,18 +1,26 @@
-//TAREA: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
-// creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
-// también vamos a crear un <h1> que diga Bienvenido!
-// vas a crear un botón de acción que una vez que lo apretás, va a
-// mostrar toda la información junta en un campo de texto
-// Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
+let $botonCalcular = document.getElementById("botonCalcular");
+$botonCalcular.addEventListener("click",ClickenCalcular);
+function ClickenCalcular(){
+    event.preventDefault(); //Esta linea nomas se agrega
 
-/*
-Ejemplo form:
-  <form id="entrada-bar" onsubmit="return false;">
-    <input type="text" placeholder="Ingresá tu nombre" id="nombre-usuario"/>
-    <input type="number" placeholder="Ingresá tu edad" id="edad-usuario" />
-    <input type="submit" value="Ingresar" id="ingresar"/>
-  </form>
+    let $nombreUsuario= document.querySelector('#nombre-usuario').value;
+    let $edadUsuario= document.querySelector("#edad-usuario").value;
+    let edadAutorizada = 18;
+    let textoResultado;    
+    
+    if ($edadUsuario >= edadAutorizada)
+    {
+       
+        textoResultado= "Hola "+$nombreUsuario +", puedes ingresar"
+    }
+    else
+    {
+        textoResultado= "Hola "+$nombreUsuario +", no puedes ingresar"
 
-  <div id="resultado">Hola</div>
-*
-* */
+    }
+    document.querySelector("#resultado1").innerText=textoResultado;
+
+}
+
+
+
